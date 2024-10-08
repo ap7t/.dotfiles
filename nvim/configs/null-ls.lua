52861341bf7fb9f8a3fce48f60ded105e60ae8de
ap7t <adampurcell7t@gmail.com>
2024-10-08 22:17:26 +0200

    update mypy config

--- nvim/configs/null-ls.lua
+++ nvim/configs/null-ls.lua
@@ -6,7 +6,12 @@ local opts = {
     null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.goimports,
     -- null_ls.builtins.formatting.golines,
-    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.mypy.with({
+      extra_args = function()
+      local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+      return { "--python-executable", virtual .. "/bin/python3" }
+      end,
+    }),
     null_ls.builtins.formatting.black,
   },
   on_attach = function(client, bufnr)
