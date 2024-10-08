81c451929ecb7d36b3895b61ff1ff7568d06d1d6
ap7t <adampurcell7t@gmail.com>
2024-10-08 22:23:39 +0200

    mypy ignore missing imports

--- nvim/configs/null-ls.lua
+++ nvim/configs/null-ls.lua
@@ -9,8 +9,12 @@ local opts = {
     null_ls.builtins.diagnostics.mypy.with({
       extra_args = function()
       local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-      return { "--python-executable", virtual .. "/bin/python3" }
+      return {
+          "--python-executable", virtual .. "/bin/python3" ,
+          "--ignore-missing-imports",
+        }
       end,
+
     }),
     null_ls.builtins.formatting.black,
   },
