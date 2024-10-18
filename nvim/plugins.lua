6bd06bc6c1beb023362aedce84246fd855577bb9
Adam Purcell <adampurcell7t@gmail.com>
2024-10-18 17:19:26 +0200

    reformatting

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -189,24 +189,24 @@ local plugins = {
         require("venv-selector").setup()
       end,
       keys = {
-        { "<leader>pv", "<cmd>VenvSelect<cr>" },
+        { ",v", "<cmd>VenvSelect<cr>" },
+      },
   },
   {
-      "folke/noice.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- add any options here
-      },
-      dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
-      },
-    }
-  },
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  }
 }
 
 
