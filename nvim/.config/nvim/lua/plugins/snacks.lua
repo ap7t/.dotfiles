8378f578b74094c5a591b02c51a74519b561d80a
ap7t <adampurcell7t@gmail.com>
2026-04-03 17:24:42 +0200

    format

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,11 +1,21 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      picker = {
-        hidden = true,
-        ignored = true,
-      },
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+      ignored = true,
     },
   },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+    local gitbrowse = require("snacks.gitbrowse")
+    local old_get_url = gitbrowse.get_url
+    gitbrowse.get_url = function(repo, fields, gopts)
+      local url = old_get_url(repo, fields, gopts)
+      if url then
+        Snacks.notify.info("Git URL generated: " .. url)
+      end
+      return url
+    end
+  end,
 }
